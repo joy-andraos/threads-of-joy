044991b7e025d186cf446c4f021f6983039aa3b5
joy-andraos <joyandraos344@gmail.com>
2025-05-14 12:01:40 +0300

    Update blog

--- src/blog-content/expectations-in-your-20s.js
+++ src/blog-content/expectations-in-your-20s.js
@@ -13,7 +13,7 @@ export default {
       <p>In light of turning 22 today, here are 5 myths about the 20s I've come to realize:</p>
 
       <h2>1. <strong>"You should have your life figured out"</strong></h2>
-      <p>The twenties are often seen as the time to establish your career, relationships, and identity. In truth, they’re more about discovering yourself. You might land what seems like your dream job only to realize it’s not what you expected. You might date someone who feels right until they’re not. The pressure feels much lighter when you view your 20s as a decade of exploration and learning from mistakes rather than a time to have everything perfectly planned.</p>
+      <p>The twenties are usually seen as the time to establish your career, relationships, and identity. In reality, they’re more about discovering yourself. You might land what seems like your dream job only to realize it’s not what you expected. You might date someone who feels right until they’re not. The pressure feels much lighter when you view your 20s as a decade of exploration and learning from mistakes rather than a time to have everything perfectly planned.</p>
 
       <h2>2. <strong>"You need to be in your dream job"</strong></h2>
       <p>It's unrealistic to believe you need to find and stick to your "forever career" in your twenties. This time is about gaining experience and trying new things to discover what really fulfills you. In fact, many people discover their passions only after years of trying the wrong jobs or gaining independence through side jobs like waitressing.</p>
