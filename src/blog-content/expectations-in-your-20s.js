650442a11e7c1fbe7c407ab291727d270aef1e8e
joy-andraos <joyandraos344@gmail.com>
2025-05-14 12:09:57 +0300

    Fix routing

--- src/blog-content/expectations-in-your-20s.js
+++ src/blog-content/expectations-in-your-20s.js
@@ -13,7 +13,7 @@ export default {
       <p>In light of turning 22 today, here are 5 myths about the 20s I've come to realize:</p>
 
       <h2>1. <strong>"You should have your life figured out"</strong></h2>
-      <p>The twenties are usually seen as the time to establish your career, relationships, and identity. In reality, they’re more about discovering yourself. You might land what seems like your dream job only to realize it’s not what you expected. You might date someone who feels right until they’re not. The pressure feels much lighter when you view your 20s as a decade of exploration and learning from mistakes rather than a time to have everything perfectly planned.</p>
+      <p>The twenties are usually seen as the time to establish your career, relationships, and identity. In reality, they're more about discovering yourself. You might land what seems like your dream job only to realize it's not what you expected. You might date someone who feels right until they're not. The pressure feels much lighter when you view your 20s as a decade of exploration and learning from mistakes rather than a time to have everything perfectly planned.</p>
 
       <h2>2. <strong>"You need to be in your dream job"</strong></h2>
       <p>It's unrealistic to believe you need to find and stick to your "forever career" in your twenties. This time is about gaining experience and trying new things to discover what really fulfills you. In fact, many people discover their passions only after years of trying the wrong jobs or gaining independence through side jobs like waitressing.</p>
@@ -25,6 +25,6 @@ export default {
       <p>Whether it's your career, education or personal goals, it's never too late to pivot. Your twenties are a great time to experiment and explore new possibilities. Sometimes, taking the wrong steps is exactly what helps you realize the right path.</p>
 
       <h2>5. <strong>"You're supposed to have all the fun now"</strong></h2>
-      <p>The pressure that you need to party or travel and live it up in your twenties can feel overwhelming. But fun doesn’t have to mean constant adventure. For some, fun looks like meaningful conversations or even working toward long-term goals. Actually, many people spend their twenties juggling multiple jobs or investing in personal growth to build a foundation for the life they truly want. Prioritizing stability or healing over wild adventures is just as fulfilling.</p>
+      <p>The pressure that you need to party or travel and live it up in your twenties can feel overwhelming. But fun doesn't have to mean constant adventure. For some, fun looks like meaningful conversations or even working toward long-term goals. Actually, many people spend their twenties juggling multiple jobs or investing in personal growth to build a foundation for the life they truly want. Prioritizing stability or healing over wild adventures is just as fulfilling.</p>
     `
 };
